a688542bf0d0e37db75d3a9d975868eeefdcc79c
gio8tisu <gio8.tisu@gmail.com>
2022-04-20 14:54:19 +0200

    Switch to postgres database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
       host: env('DATABASE_HOST'),
       port: env('DATABASE_PORT'),
       database: env('DATABASE_DATABASE'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
-      ssl: "Amazon RDS",
+      ssl: false,
     }
   }
 });
