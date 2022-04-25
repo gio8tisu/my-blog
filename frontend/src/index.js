f9cad7810c0f0bc809524967046ade717f6e35aa
gio8tisu <gio8.tisu@gmail.com>
2022-04-25 19:15:28 +0200

    Update react root thingy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { ApolloProvider } from "@apollo/client";
@@ -10,11 +10,12 @@ import client from "./utils/apolloClient";
 
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <Router>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
