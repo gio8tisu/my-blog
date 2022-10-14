2704f89d7cf21b949448b968e0f32435294f0de1
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-10-14 14:25:20 +0200

    Bump up strapi to version 4.4.3

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '2eb67ceb90482cb5cfe6fb9cf08b840e'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
